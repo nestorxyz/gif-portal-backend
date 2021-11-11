727977571c4d6db3b418765da16526262f798b43
Nestor Mamani <nmamanipantoja@gmail.com>
2021-11-10 22:41:46 -0500

    ⚡️ Store basic data on our contract.

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -7,9 +7,35 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod myepicproject {
     use super::*;
     pub fn start_stuff_off(ctx:Context<StartStuffOff>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info>{
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount{
+    pub total_gifs: u64,
+}
--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,13 +1,39 @@
 const anchor = require('@project-serum/anchor');
 
+const { SystemProgram } = anchor.web3;
+
 const main = async () => {
   console.log('🚀 Starting test...');
 
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log('📝 Your transaction signature', tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('🎇 Gif Count', account.totalGifs.toString());
+
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('🎇 GIF Count', account.totalGifs.toString());
 };
 
 const runMain = async () => {
